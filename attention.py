764d044fac1d643b3d04ceceb95e23519029ea09
drdavis65 <92239386+drdavis65@users.noreply.github.com>
2025-07-11 15:03:54 -0700

    SingleHeadSelfAttention implementation

--- attention.py
+++ attention.py
@@ -1,7 +1,11 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torchtyping import TensorType
 
+N = 6 # number of layers
+h = 8 # number of heads
+d_model = 512 # embedding dimensions
 
 class SingleHeadSelfAttention(nn.Module):
         def __init__(self, embedding_dim : int, head_size : int):
@@ -10,6 +14,24 @@ class SingleHeadSelfAttention(nn.Module):
             self.get_querys = nn.Linear(embedding_dim, head_size, bias = False)
             self.get_values = nn.Linear(embedding_dim, head_size, bias = False)
 
-        def forward(self, q, k, v, head_size, mask=None) -> TensorType[float]:
-              
+        def forward(self, q, k_v, mask=None) -> TensorType[float]:
+            # Batch Size x Context (Sequence Length) x Embedding Dimensions
+            K = self.get_keys(k_v)
+            Q = self.get_querys(q)
+            V = self.get_values(k_v)
+
+            scores = (Q @ K.transpose(-2, -1)) / (K.shape[2] ** 0.5)
+
+            scores = scores.masked_fill(mask, float('inf'))
+            attn_weights = F.softmax(dim=-1)
+
+            output = attn_weights @ V
+
+            return output
+
+
+
+
+
+
     
